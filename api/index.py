14c56fb71cfcc47164e1656a51cebc4fa92e15c1
Ronakon Watt <88375612+ronakonwatta@users.noreply.github.com>
2024-06-18 11:03:13 +0700

    /form to /cal

--- api/index.py
+++ api/index.py
@@ -16,3 +16,19 @@ def api():
 <h1>HTML</h1>
 
 '''
+
+@app.route('/form')
+def form():
+    return '''
+<form action="/cal" method="post">
+  <input type="text" name="x">
+  <input type="text" name="y">
+  <input type="submit" value="Submit">
+</form>
+'''
+
+@app.route('/cal', methods=['POST'])
+def cal():
+    x = request.form['x']
+    y = request.form['y']
+    return str(int(x) + int(y))
